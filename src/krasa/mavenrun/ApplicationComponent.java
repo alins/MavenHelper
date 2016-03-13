d327bfd765deb5091ca1848ff001f0be11244e1f
Alin Simionoiu <alin@Alin-MacBook-Pro.local>
2016-03-13 11:25:19 -0700

    prep for intellij 16

--- src/krasa/mavenrun/ApplicationComponent.java
+++ src/krasa/mavenrun/ApplicationComponent.java
@@ -29,160 +29,160 @@ import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
 import icons.MavenIcons;
 
-@State(name = "MavenRunHelper", storages = { @Storage(id = "MavenRunHelper", file = "$APP_CONFIG$/mavenRunHelper.xml") })
+@State(name = "MavenRunHelper", storages = {@Storage(id = "MavenRunHelper", file = "$APP_CONFIG$/mavenRunHelper.xml")})
 public class ApplicationComponent implements com.intellij.openapi.components.ApplicationComponent, Configurable,
-		PersistentStateComponent<ApplicationSettings> {
-	static final Logger LOG = Logger.getInstance(ApplicationComponent.class);
+    PersistentStateComponent<ApplicationSettings> {
+    static final Logger LOG = Logger.getInstance(ApplicationComponent.class);
 
-	public static final String RUN_MAVEN = "Run Maven";
-	public static final String DEBUG_MAVEN = "Debug Maven";
-	private ApplicationSettingsForm form;
-	private ApplicationSettings settings = ApplicationSettings.defaultApplicationSettings();
+    public static final String RUN_MAVEN = "Run Maven";
+    public static final String DEBUG_MAVEN = "Debug Maven";
+    private ApplicationSettingsForm form;
+    private ApplicationSettings settings = ApplicationSettings.defaultApplicationSettings();
 
-	public void initComponent() {
-		addActionGroup(new MainMavenDebugActionGroup(DEBUG_MAVEN, Debug.ICON), RUN_MAVEN);
-		addActionGroup(new MainMavenActionGroup(RUN_MAVEN, MavenIcons.Phase), RUN_MAVEN);
-		registerActions();
-	}
+    public void initComponent() {
+        addActionGroup(new MainMavenDebugActionGroup(DEBUG_MAVEN, Debug.ICON), RUN_MAVEN);
+        addActionGroup(new MainMavenActionGroup(RUN_MAVEN, MavenIcons.Phase), RUN_MAVEN);
+        registerActions();
+    }
 
-	private void registerActions() {
-		ActionManager instance = ActionManager.getInstance();
-		for (Goal goal : settings.getAllGoals()) {
-			String actionId = getActionId(goal);
-			registerAction(instance, actionId, new RunGoalAction(goal, MavenIcons.PluginGoal));
-			actionId = getDebugActionId(goal);
-			registerAction(instance, actionId, new DebugGoalAction(goal, MavenIcons.PluginGoal));
-		}
-		registerAction(instance, "krasa.MavenHelper.RunTestFileAction", new RunTestFileAction());
-		registerAction(instance, "krasa.MavenHelper.DebugTestFileAction", new DebugTestFileAction());
-	}
+    private void registerActions() {
+        ActionManager instance = ActionManager.getInstance();
+        for (Goal goal : settings.getAllGoals()) {
+            String actionId = getActionId(goal);
+            registerAction(instance, actionId, new RunGoalAction(goal, MavenIcons.PluginGoal));
+            actionId = getDebugActionId(goal);
+            registerAction(instance, actionId, new DebugGoalAction(goal, MavenIcons.PluginGoal));
+        }
+        registerAction(instance, "krasa.MavenHelper.RunTestFileAction", new RunTestFileAction());
+        registerAction(instance, "krasa.MavenHelper.DebugTestFileAction", new DebugTestFileAction());
+    }
 
-	private void unRegisterActions() {
-		ActionManager instance = ActionManager.getInstance();
-		for (Goal goal : settings.getAllGoals()) {
-			unRegisterAction(instance, getActionId(goal));
-			unRegisterAction(instance, getDebugActionId(goal));
-		}
-	}
+    private void unRegisterActions() {
+        ActionManager instance = ActionManager.getInstance();
+        for (Goal goal : settings.getAllGoals()) {
+            unRegisterAction(instance, getActionId(goal));
+            unRegisterAction(instance, getDebugActionId(goal));
+        }
+    }
 
-	public void registerAction(Goal o, RunGoalAction runGoalAction) {
-		ActionManager instance = ActionManager.getInstance();
-		registerAction(instance, getActionId(o), runGoalAction);
-	}
+    public void registerAction(Goal o, RunGoalAction runGoalAction) {
+        ActionManager instance = ActionManager.getInstance();
+        registerAction(instance, getActionId(o), runGoalAction);
+    }
 
-	private void registerAction(ActionManager instance, String actionId1, AnAction runGoalAction) {
-		unRegisterAction(instance, actionId1);
-		instance.registerAction(actionId1, runGoalAction, PluginId.getId("MavenRunHelper"));
-	}
+    private void registerAction(ActionManager instance, String actionId1, AnAction runGoalAction) {
+        unRegisterAction(instance, actionId1);
+        instance.registerAction(actionId1, runGoalAction, PluginId.getId("MavenRunHelper"));
+    }
 
-	private void unRegisterAction(ActionManager instance, String actionId) {
-		instance.unregisterAction(actionId);
-	}
+    private void unRegisterAction(ActionManager instance, String actionId) {
+        instance.unregisterAction(actionId);
+    }
 
-	private String getActionId(Goal goal) {
-		return "MavenRunHelper" + WordUtils.capitalizeFully(goal.getCommandLine()).replaceAll(" ", "");
-	}
+    private String getActionId(Goal goal) {
+        return "MavenRunHelper" + WordUtils.capitalizeFully(goal.getCommandLine()).replaceAll(" ", "");
+    }
 
-	private String getDebugActionId(Goal goal) {
-		return "MavenRunHelperDebug" + WordUtils.capitalizeFully(goal.getCommandLine()).replaceAll(" ", "");
+    private String getDebugActionId(Goal goal) {
+        return "MavenRunHelperDebug" + WordUtils.capitalizeFully(goal.getCommandLine()).replaceAll(" ", "");
 
-	}
+    }
 
-	private void addActionGroup(ActionGroup actionGroup, String runMaven) {
-		DefaultActionGroup editorPopupMenu = (DefaultActionGroup) ActionManager.getInstance().getAction(
-				"EditorPopupMenu.Run");
-		DefaultActionGroup projectViewPopupMenuRunGroup = (DefaultActionGroup) ActionManager.getInstance().getAction(
-				"ProjectViewPopupMenuRunGroup");
-		clear(editorPopupMenu, projectViewPopupMenuRunGroup, runMaven);
+    private void addActionGroup(ActionGroup actionGroup, String runMaven) {
+        DefaultActionGroup editorPopupMenu = (DefaultActionGroup) ActionManager.getInstance().getAction(
+            "EditorPopupMenu.Run");
+        DefaultActionGroup projectViewPopupMenuRunGroup = (DefaultActionGroup) ActionManager.getInstance().getAction(
+            "ProjectViewPopupMenuRunGroup");
+        clear(editorPopupMenu, projectViewPopupMenuRunGroup, runMaven);
 
-		add(actionGroup, editorPopupMenu, projectViewPopupMenuRunGroup);
-	}
+        add(actionGroup, editorPopupMenu, projectViewPopupMenuRunGroup);
+    }
 
-	private void add(ActionGroup actionGroup, DefaultActionGroup editorPopupMenu,
-			DefaultActionGroup projectViewPopupMenuRunGroup) {
-		editorPopupMenu.add(actionGroup, Constraints.FIRST);
-		projectViewPopupMenuRunGroup.add(actionGroup, Constraints.FIRST);
-	}
+    private void add(ActionGroup actionGroup, DefaultActionGroup editorPopupMenu,
+                     DefaultActionGroup projectViewPopupMenuRunGroup) {
+        editorPopupMenu.add(actionGroup, Constraints.FIRST);
+        projectViewPopupMenuRunGroup.add(actionGroup, Constraints.FIRST);
+    }
 
-	private void clear(DefaultActionGroup editorPopupMenu, DefaultActionGroup projectViewPopupMenuRunGroup,
-			String runMaven) {
-		clear(editorPopupMenu, RUN_MAVEN);
-		clear(projectViewPopupMenuRunGroup, runMaven);
-	}
+    private void clear(DefaultActionGroup editorPopupMenu, DefaultActionGroup projectViewPopupMenuRunGroup,
+                       String runMaven) {
+        clear(editorPopupMenu, RUN_MAVEN);
+        clear(projectViewPopupMenuRunGroup, runMaven);
+    }
 
-	private void clear(DefaultActionGroup editorPopupMenu, String runMaven) {
-		AnAction[] childActionsOrStubs = editorPopupMenu.getChildActionsOrStubs();
-		for (AnAction childActionsOrStub : childActionsOrStubs) {
-			if (runMaven.equals(childActionsOrStub.getTemplatePresentation().getText())) {
-				editorPopupMenu.remove(childActionsOrStub);
-			}
-		}
-	}
+    private void clear(DefaultActionGroup editorPopupMenu, String runMaven) {
+        AnAction[] childActionsOrStubs = editorPopupMenu.getChildActionsOrStubs();
+        for (AnAction childActionsOrStub : childActionsOrStubs) {
+            if (runMaven.equals(childActionsOrStub.getTemplatePresentation().getText())) {
+                editorPopupMenu.remove(childActionsOrStub);
+            }
+        }
+    }
 
-	public void disposeComponent() {
-		// TODO: insert component disposal logic here
-	}
+    public void disposeComponent() {
+        // TODO: insert component disposal logic here
+    }
 
-	@NotNull
-	public String getComponentName() {
-		return "ApplicationComponent";
-	}
+    @NotNull
+    public String getComponentName() {
+        return "ApplicationComponent";
+    }
 
-	@NotNull
-	@Override
-	public ApplicationSettings getState() {
-		if (settings == null) {
-			settings = ApplicationSettings.defaultApplicationSettings();
-		}
-		return settings;
-	}
+    @NotNull
+    @Override
+    public ApplicationSettings getState() {
+        if (settings == null) {
+            settings = ApplicationSettings.defaultApplicationSettings();
+        }
+        return settings;
+    }
 
-	public void loadState(ApplicationSettings state) {
-		if (state.getVersion() < ApplicationSettings.ACTUAL_VERSION) {
-			settings = ApplicationSettings.defaultApplicationSettings();
-		} else {
-			settings = state;
-		}
-	}
+    public void loadState(ApplicationSettings state) {
+        if (state.getVersion() < ApplicationSettings.ACTUAL_VERSION) {
+            settings = ApplicationSettings.defaultApplicationSettings();
+        } else {
+            settings = state;
+        }
+    }
 
-	@Nls
-	public String getDisplayName() {
-		return "Maven Helper";
-	}
+    @Nls
+    public String getDisplayName() {
+        return "Maven Helper";
+    }
 
-	@Nullable
-	public String getHelpTopic() {
-		return null;
-	}
+    @Nullable
+    public String getHelpTopic() {
+        return null;
+    }
 
-	@Nullable
-	public JComponent createComponent() {
-		form = new ApplicationSettingsForm(getState());
-		return form.getRootComponent();
-	}
+    @Nullable
+    public JComponent createComponent() {
+        form = new ApplicationSettingsForm(getState());
+        return form.getRootComponent();
+    }
 
-	public boolean isModified() {
-		return form.isSettingsModified(settings);
-	}
+    public boolean isModified() {
+        return form.isSettingsModified(settings);
+    }
 
-	public void apply() throws ConfigurationException {
-		unRegisterActions();
-		settings = form.getSettings().clone();
-		registerActions();
-	}
+    public void apply() throws ConfigurationException {
+        unRegisterActions();
+        settings = form.getSettings().clone();
+        registerActions();
+    }
 
-	public void reset() {
-		if (form != null) {
-			form.importFrom(settings);
-		}
-	}
+    public void reset() {
+        if (form != null) {
+            form.importFrom(settings);
+        }
+    }
 
-	public void disposeUIResources() {
-		form = null;
-	}
+    public void disposeUIResources() {
+        form = null;
+    }
 
-	public static ApplicationComponent getInstance() {
-		return ApplicationManager.getApplication().getComponent(ApplicationComponent.class);
-	}
+    public static ApplicationComponent getInstance() {
+        return ApplicationManager.getApplication().getComponent(ApplicationComponent.class);
+    }
 
 }
--- src/krasa/mavenrun/action/RunTestFileActionTest.java
+++ src/krasa/mavenrun/action/RunTestFileActionTest.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 
 public class RunTestFileActionTest {
 
-	@Test
-	public void testMatchClassRegexPatter() throws Exception {
-		assertTrue(RunTestFileAction.matchClassRegexPatter("com/foo/FailsafeSampleIT.java", "**/*IT.java"));
-		assertFalse(RunTestFileAction.matchClassRegexPatter("com/foo/FailsafeSampleTest.java", "**/*IT.java"));
-	}
+    @Test
+    public void testMatchClassRegexPatter() throws Exception {
+        assertTrue(RunTestFileAction.matchClassRegexPatter("com/foo/FailsafeSampleIT.java", "**/*IT.java"));
+        assertFalse(RunTestFileAction.matchClassRegexPatter("com/foo/FailsafeSampleTest.java", "**/*IT.java"));
+    }
 }
